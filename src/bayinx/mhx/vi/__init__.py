41e2f9abc188b31cf9630f7ed541c4e46b14cc78
Todd McCready <175446079+toddmccready@users.noreply.github.com>
2025-04-13 14:32:00 -0400

    ruff format

--- src/bayinx/mhx/vi/__init__.py
+++ src/bayinx/mhx/vi/__init__.py
@@ -1,5 +1,3 @@
 from bayinx.mhx.vi.meanfield import MeanField as MeanField
-from bayinx.mhx.vi.normalizing_flow import (
-    NormalizingFlow as NormalizingFlow,
-)
+from bayinx.mhx.vi.normalizing_flow import NormalizingFlow as NormalizingFlow
 from bayinx.mhx.vi.standard import Standard as Standard
