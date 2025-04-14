d17a5d248791c316287334ca2cfc7ad92e3ed594
Todd McCready <175446079+toddmccready@users.noreply.github.com>
2025-04-14 15:06:26 -0400

    fix eval vmap for normalizing flows

--- src/bayinx/mhx/vi/normalizing_flow.py
+++ src/bayinx/mhx/vi/normalizing_flow.py
@@ -1,6 +1,8 @@
+from functools import partial
 from typing import Any, Callable, Self, Tuple
 
 import equinox as eqx
+import jax
 import jax.flatten_util as jfu
 import jax.numpy as jnp
 import jax.random as jr
@@ -59,6 +61,7 @@ class NormalizingFlow(Variational):
         return draws
 
     @eqx.filter_jit
+    @partial(jax.vmap, in_axes=(None, 0))
     def eval(self, draws: Array) -> Array:
         # Evaluate base density
         variational_evals: Array = self.base.eval(draws)
