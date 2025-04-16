fixed jacobian notion

src/bayinx/mhx/vi/flows/fullaffine.py
@@ -35,6 +35,7 @@ class FullAffine(Flow):
 
         self.constraints = {"scale": lambda m: jnp.tril(m)}
 
+    @eqx.filter_jit
     def transform_pars(self):
         # Get constrained parameters
         params = self.constrain_pars()
@@ -75,7 +76,7 @@ class FullAffine(Flow):
         # Compute forward transformation
         draws = draws @ scale + shift
 
-        # Compute ladj
-        ladj: Scalar = jnp.log(jnp.diag(scale)).sum()
+        # Compute laj
+        laj: Scalar = jnp.log(jnp.diag(scale)).sum()
 
-        return ladj, draws
+        return laj, draws

src/bayinx/mhx/vi/flows/radial.py
@@ -84,11 +84,11 @@ class Radial(Flow):
         draws = draws + (x) * (draws - center)
 
         # Compute density adjustment
-        ladj = jnp.log(
+        laj = jnp.log(
             jnp.abs(
                 (1.0 + alpha * beta / (alpha + r) ** 2.0)
                 * (1.0 + x) ** (center.size - 1.0)
             )
         )
 
-        return ladj, draws
+        return laj, draws